9096069e23e1d686cf0a252551137b0751d355dd
Jesper Sørensen <jeppester1@gmail.com>
2025-03-30 18:42:08 +0000

    Fix indentation

--- templates/error.php
+++ templates/error.php
@@ -1,8 +1,8 @@
 <?php declare(strict_types=1); ?>
 
 <div class="app-content">
-    <h2>Photo Frames</h2>
-    <p class="alert alert--danger">
-        <?= $message ?>
-    </p>
+  <h2>Photo Frames</h2>
+  <p class="alert alert--danger">
+    <?= $message ?>
+  </p>
 </div>
