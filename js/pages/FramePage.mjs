57938392c11e91dcffc4df4b8e5c514b9cf35ec5
Jesper Sørensen <jeppester1@gmail.com>
2025-06-03 21:19:00 +0000

    Update date styling

--- js/pages/FramePage.mjs
+++ js/pages/FramePage.mjs
@@ -30,15 +30,32 @@ injectGlobal`
 `;
 
 const styles = {
-  photoFrame: css`
+  frame: css`
     animation: ${animations.fadeIn} 2s ease-in-out;
-    background-color: #1c1c1c;
+    background-color: #000;
     position: absolute;
     width: 100vw;
     height: 100vh;
-    background-position: center center;
-    background-repeat: no-repeat;
+  `,
+  photoBackground: css`
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-position: center;
+    background-size: 100% 100%;
+    filter: blur(100px) brightness(70%);
+  `,
+  photo: css`
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-position: center;
     background-size: contain;
+    background-repeat: no-repeat;
   `,
   dateContainer: css`
     position: absolute;
@@ -46,23 +63,46 @@ const styles = {
     left: 0;
     right: 0;
     display: flex;
-    justify-content: center;
-    padding: 0.3rem;
+    justify-content: start;
+    padding: 1rem;
+  `,
+  dateBackground: css`
+    border-radius: 1rem;
+    border-top-left-radius: 0;
+    border-bottom-right-radius: 0;
+    padding: 1rem 1rem;
+    display: flex;
+    flex-direction: column;
+    align-items: start;
+    background-color: rgba(255, 250, 250, 0.4);
+    font-size: 2rem;
+    border: 1px solid rgba(213, 204, 195, 0.3);
+    box-shadow: 0px 5px 40px rgba(0, 0, 0, 0.2);
+    backdrop-filter: blur(5px);
   `,
   date: css`
+    text-align: left;
     text-transform: capitalize;
-    font-family: Georgia, "Times New Roman", Times, serif;
-    font-weight: 400;
-    border-radius: 0.2rem;
+    font-family: sans-serif;
     margin: 0;
-    padding: 0.2rem 0.65rem 0.2rem;
-    background-color: rgba(0, 0, 0, 0.6);
-    font-size: 1rem;
-    color: rgb(200, 194, 189);
-    border: 1px solid rgba(213, 204, 195, 0.3);
-    outline: 1px solid rgba(0, 0, 0, 0.6);
-    text-shadow: 0px 1px 0px black;
-    box-shadow: 0px 5px 40px rgba(0, 0, 0, 0.2);
+    color: rgb(35, 18, 5);
+    text-shadow: 0px 0px 1px rgba(255, 255, 255, 0.4);
+  `,
+  dateSpacer: css`
+    height: 1.5px;
+    margin: 0.25rem 1rem 0 0.1rem;
+    background-color: rgb(35, 18, 5);
+  `,
+  year: css`
+    font-size: 2rem;
+    font-weight: 600;
+  `,
+  month: css`
+    font-size: 1.2rem;
+    font-weight: 500;
+    margin-left: 0.05rem;
+    padding-bottom: 0.2rem;
+    border-bottom: 1.5px solid rgb(35, 18, 5);
   `,
 };
 
@@ -128,20 +168,31 @@ export default function FramePage(props) {
   return html`
     ${images.map(
       (image) => html`
-        <div
-          className=${styles.photoFrame}
-          key=${image.expiresAt}
-          style=${{ backgroundImage: `url('${image.url}')` }}
-        >
+        <div className=${styles.frame} key=${image.expiresAt}>
+          <div
+            className=${styles.photoBackground}
+            style=${{ backgroundImage: `url("${image.url}")` }}
+          />
+          <div
+            className=${styles.photo}
+            style=${{ backgroundImage: `url("${image.url}")` }}
+          />
+
           ${showPhotoTimestamp &&
           html`
             <div className=${styles.dateContainer}>
-              <h1 className=${styles.date}>
-                ${Intl.DateTimeFormat(navigator.locale, {
-                  month: "long",
-                  year: "numeric",
-                }).format(image.timestamp)}
-              </h1>
+              <div className=${styles.dateBackground}>
+                <h1 className=${`${styles.date} ${styles.month}`}>
+                  ${Intl.DateTimeFormat(navigator.locale, {
+                    month: "short",
+                  }).format(image.timestamp)}
+                </h1>
+                <h1 className=${`${styles.date} ${styles.year}`}>
+                  ${Intl.DateTimeFormat(navigator.locale, {
+                    year: "numeric",
+                  }).format(image.timestamp)}
+                </h1>
+              </div>
             </div>
           `}
         </div>
