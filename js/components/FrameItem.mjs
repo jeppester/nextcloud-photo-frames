4c2f7b6dae291dcff5fed89fe8012f2583f904e2
Jesper Sørensen <jeppester1@gmail.com>
2025-06-07 18:15:39 +0000

    Extract FrameItem component from IndexPage

---
+++ js/components/FrameItem.mjs
@@ -0,0 +1,136 @@
+import { html } from "../vendor/htm-preact-standalone.min.mjs";
+import { css } from "../vendor/emotion-css.min.mjs";
+import { generateUrl } from "../vendor/nextcloud-router.min.mjs";
+
+import CopyButton from "../components/CopyButton.mjs";
+import Actions from "../components/Actions.mjs";
+
+const urlForFrame = ({ shareToken }) =>
+  location.origin +
+  generateUrl("apps/photo_frames/{shareToken}", { shareToken });
+
+const styles = {
+  frame: css`
+    border: 2px solid #00679e88;
+    background-color: #00679e09;
+    box-shadow: 0px 5px 10px rgba(0, 0, 0, 0.2);
+    border-radius: 1rem;
+    padding: 1.5rem;
+    display: flex;
+    gap: 2rem;
+    overflow: hidden;
+
+    @media (max-width: calc(550px + 3rem)) {
+      flex-direction: column;
+    }
+
+    img {
+      width: 250px;
+      object-fit: cover;
+      margin: -1.5rem;
+      margin-right: 0;
+
+      @media (max-width: calc(550px + 3rem)) {
+        width: calc(100% + 3rem);
+        margin-right: -1.5rem;
+        margin-bottom: 0;
+        aspect-ratio: 16/9;
+      }
+    }
+
+    h2 {
+      font-size: 1.5rem;
+      font-weight: 500;
+      margin: 0 0 0.5rem;
+    }
+
+    p {
+      font-size: 16px;
+      font-weight: 500;
+      margin: 0;
+    }
+  `,
+};
+
+export default function FrameItem(props) {
+  const { frame, onShowQRCode, onDelete } = props;
+
+  return html`
+    <div className=${styles.frame}>
+      <img
+        src=${generateUrl("apps/photo_frames/{shareToken}/image", {
+          shareToken: frame.shareToken,
+        })}
+      />
+      <div className="grow">
+        <div className="flex">
+          <h2 className="grow">${frame.name}</h2>
+
+          <button className="error" onClick=${() => onDelete(frame)}>
+            Delete
+          </button>
+        </div>
+        <p><strong>Album:</strong> ${frame.albumName}</p>
+
+        <p>
+          <strong>Select:</strong>
+          ${" "}
+          ${{
+            latest: "Latest",
+            oldest: "Oldest",
+            random: "Random",
+          }[frame.selectionMethod]}
+        </p>
+        <p>
+          <strong>Rotation: </strong>
+          ${frame.rotationsPerUnit}
+          ${frame.rotationsPerUnit === 1 ? " photo " : " photos "} per ${" "}
+          ${{
+            day: "day",
+            hour: "hour",
+            minute: "minute",
+          }[frame.rotationUnit]}
+        </p>
+        <p>
+          <strong>Start day at:</strong>
+          ${" "}${frame.startDayAt}
+        </p>
+        <p>
+          <strong>End day at:</strong>
+          ${" "}${frame.endDayAt}
+        </p>
+        <p>
+          <strong>Show date:</strong>
+          ${" "}${frame.showPhotoTimestamp ? "Enabled" : "Disabled"}
+        </p>
+        <${Actions}>
+          <a
+            target="_BLANK"
+            href=${generateUrl("apps/photo_frames/{shareToken}", {
+              shareToken: frame.shareToken,
+            })}
+          >
+            <button>Show</button>
+          </a>
+          <a
+            href=${generateUrl("apps/photo_frames/{id}/edit", {
+              id: frame.id,
+            })}
+          >
+            <button>Edit</button>
+          </a>
+          <button onClick=${() => onShowQRCode(urlForFrame(frame))}>
+            Show QR
+          </button>
+          <${CopyButton}
+            className="primary"
+            data=${urlForFrame(frame)}
+            copiedText="Copied"
+          >
+            Copy link
+          <//>
+        <//>
+      </div>
+    </div>
+  `;
+}
--- js/pages/IndexPage.mjs
+++ js/pages/IndexPage.mjs
@@ -6,8 +6,7 @@ import {
 import { css } from "../vendor/emotion-css.min.mjs";
 import { generateUrl } from "../vendor/nextcloud-router.min.mjs";
 
-import CopyButton from "../components/CopyButton.mjs";
-import Actions from "../components/Actions.mjs";
+import FrameItem from "../components/FrameItem.mjs";
 
 const styles = {
   list: css`
@@ -25,53 +24,6 @@ const styles = {
       gap: 1.5rem;
       margin-top: 1rem;
       margin-bottom: 1rem;
-    }
-  `,
-  frame: css`
-    border: 2px solid #00679e88;
-    background-color: #00679e09;
-    box-shadow: 0px 5px 10px rgba(0, 0, 0, 0.2);
-    border-radius: 1rem;
-    padding: 1.5rem;
-    display: flex;
-    gap: 2rem;
-    overflow: hidden;
-
-    @media (max-width: calc(550px + 3rem)) {
-      flex-direction: column;
-    }
-
-    img {
-      width: 250px;
-      object-fit: cover;
-      margin: -1.5rem;
-      margin-right: 0;
-
-      @media (max-width: calc(550px + 3rem)) {
-        width: calc(100% + 3rem);
-        margin-right: -1.5rem;
-        margin-bottom: 0;
-        aspect-ratio: 16/9;
-      }
-    }
-
-    h2 {
-      font-size: 1.5rem;
-      font-weight: 500;
-      margin: 0 0 0.5rem;
-    }
-
-    p {
-      font-size: 16px;
-      font-weight: 500;
-      margin: 0;
-    }
-
-    .actions {
-      display: flex;
-      flex-wrap: wrap;
-      margin-top: 1rem;
-      gap: 0.5rem;
     }
   `,
   modal: css`
@@ -102,10 +54,6 @@ const styles = {
   `,
 };
 
-const urlForFrame = ({ shareToken }) =>
-  location.origin +
-  generateUrl("apps/photo_frames/{shareToken}", { shareToken });
-
 export default function IndexPage(props) {
   const [modalShown, setModalShown] = useState(false);
   const modalRef = useRef();
@@ -113,7 +61,7 @@ export default function IndexPage(props) {
   // Store frames in state so that we can update the UI when a frame is deleted
   const [frames, setFrames] = useState(props.frames);
 
-  const showQRCode = (frame) => {
+  const showQRCode = (url) => {
     const modalContent = modalRef.current.querySelector(".content");
     modalContent.innerHTML = "";
 
@@ -122,7 +70,7 @@ export default function IndexPage(props) {
     modalContent.append(div);
 
     setModalShown(true);
-    new QRCode(div, urlForFrame(frame));
+    new QRCode(div, url);
   };
 
   const closeModal = () => setModalShown(false);
@@ -148,85 +96,13 @@ export default function IndexPage(props) {
     </div>
 
     <div className=${styles.list}>
-      ${frames.map((frame) => {
-        return html`
-          <div className=${styles.frame}>
-            <img
-              src=${generateUrl("apps/photo_frames/{shareToken}/image", {
-                shareToken: frame.shareToken,
-              })}
-            />
-            <div className="grow">
-              <div className="flex">
-                <h2 className="grow">${frame.name}</h2>
-
-                <button className="error" onClick=${() => deleteFrame(frame)}>
-                  Delete
-                </button>
-              </div>
-              <p><strong>Album:</strong> ${frame.albumName}</p>
-
-              <p>
-                <strong>Select:</strong>
-                ${" "}
-                ${{
-                  latest: "Latest",
-                  oldest: "Oldest",
-                  random: "Random",
-                }[frame.selectionMethod]}
-              </p>
-              <p>
-                <strong>Rotation: </strong>
-                ${frame.rotationsPerUnit}
-                ${frame.rotationsPerUnit === 1 ? " photo " : " photos "} per
-                ${" "}
-                ${{
-                  day: "day",
-                  hour: "hour",
-                  minute: "minute",
-                }[frame.rotationUnit]}
-              </p>
-              <p>
-                <strong>Start day at:</strong>
-                ${" "}${frame.startDayAt}
-              </p>
-              <p>
-                <strong>End day at:</strong>
-                ${" "}${frame.endDayAt}
-              </p>
-              <p>
-                <strong>Show date:</strong>
-                ${" "}${frame.showPhotoTimestamp ? "Enabled" : "Disabled"}
-              </p>
-              <${Actions}>
-                <a
-                  target="_BLANK"
-                  href=${generateUrl("apps/photo_frames/{shareToken}", {
-                    shareToken: frame.shareToken,
-                  })}
-                >
-                  <button>Show</button>
-                </a>
-                <a
-                  href=${generateUrl("apps/photo_frames/{id}/edit", {
-                    id: frame.id,
-                  })}
-                >
-                  <button>Edit</button>
-                </a>
-                <button onClick=${() => showQRCode(frame)}>Show QR</button>
-                <${CopyButton}
-                  className="primary"
-                  data=${urlForFrame(frame)}
-                  copiedText="Copied"
-                >
-                  Copy link
-                <//>
-              <//>
-            </div>
-          </div>
-        `;
-      })}
+      ${frames.map(
+        (frame) => html`<${FrameItem}
+          frame=${frame}
+          onShowQRCode=${showQRCode}
+          onDelete=${deleteFrame}
+        />`
+      )}
     </div>
 
     <div
