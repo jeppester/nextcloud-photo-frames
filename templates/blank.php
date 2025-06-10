34126beb74366c7adb30e3cc8eb548e9d78af9a4
Jesper Sørensen <jeppester1@gmail.com>
2025-06-10 20:11:01 +0000

    Templates: Use $_['VAR'] instead of $VAR

--- templates/blank.php
+++ templates/blank.php
@@ -3,13 +3,13 @@
 
 <head>
   <script nonce="<?= $_['cspNonce']; ?>">
-    window.appPath = "<?= $appPath ?>"
+    window.appPath = "<?= $_['appPath'] ?>"
   </script>
   <script type="module" nonce="<?= $_['cspNonce']; ?>">
-    import { html, render } from "<?= $appPath ?>/js/vendor/htm-preact-standalone.min.mjs";
-    import Page from "<?= $appPath ?>/js/pages/<?= $pageName ?>.mjs"
+    import { html, render } from "<?= $_['appPath']; ?>/js/vendor/htm-preact-standalone.min.mjs";
+    import Page from "<?= $_['appPath'] ?>/js/pages/<?= $_['pageName'] ?>.mjs"
 
-    const pageProps = <?= json_encode($pageProps, JSON_HEX_TAG | JSON_THROW_ON_ERROR) ?>;
+    const pageProps = <?= json_encode($_['pageProps'], JSON_HEX_TAG | JSON_THROW_ON_ERROR) ?>;
     document.addEventListener('DOMContentLoaded', () => {
       render(html`<${Page} ...${pageProps} />`, document.querySelector('body'));
     })
--- templates/page.php
+++ templates/page.php
@@ -4,13 +4,13 @@
 </div>
 
 <script nonce="<?= $_['cspNonce']; ?>">
-  window.appPath = "<?= $appPath ?>"
+  window.appPath = "<?= $_['appPath'] ?>"
 </script>
 <script type="module" nonce="<?= $_['cspNonce']; ?>">
-  import { html, render } from "<?= $appPath ?>/js/vendor/htm-preact-standalone.min.mjs";
+  import { html, render } from "<?= $_['appPath'] ?>/js/vendor/htm-preact-standalone.min.mjs";
 
-  const pageProps = <?= json_encode($pageProps, JSON_HEX_TAG | JSON_THROW_ON_ERROR) ?>;
+  const pageProps = <?= json_encode($_['pageProps'], JSON_HEX_TAG | JSON_THROW_ON_ERROR) ?>;
 
-  import Page from "<?= $appPath ?>/js/pages/<?= $pageName ?>.mjs"
+  import Page from "<?= $_['appPath'] ?>/js/pages/<?= $_['pageName'] ?>.mjs"
   render(html`<${Page} ...${pageProps} />`, document.querySelector('.app-content'));
 </script>
